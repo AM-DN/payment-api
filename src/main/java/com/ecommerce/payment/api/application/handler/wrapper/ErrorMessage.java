0dc294e2cbfe91a3c14d0590afe3e02b2eb943fc
Jefferson Alves <jeffersonalmeida16@outlook.com>
2023-05-03 21:19:52 -0300

    project structure

---
+++ src/main/java/com/ecommerce/payment/api/application/handler/wrapper/ErrorMessage.java
@@ -0,0 +1,9 @@
+package com.ecommerce.payment.api.application.handler.wrapper;
+
+public class ErrorMessage {
+
+    private ErrorMessage() {
+        throw new UnsupportedOperationException("This a utility class and cannot be instantiated");
+    }
+
+}
